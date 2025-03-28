b10a5349d9616ec32ab379fb3d77892cf21c1cf1
Michael-Ngochi <ngoochimike@gmail.com>
2025-03-29 01:43:58 +0300

    added more coments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 
 document.addEventListener("DOMContentLoaded",()=>{
-
+//function fo adding show to favourites
 async function addToFavourites(show){
-show.id = String(show.id);
+show.id = String(show.id); //convert id to string
 let res= await fetch("http://localhost:3000/favourites",{
      method: 'POST',
      headers: {
@@ -16,34 +16,36 @@ let res= await fetch("http://localhost:3000/favourites",{
 
 const data = await res.json();
 alert(`${show.name} has been added to your favourites!`)
-showFavourites();
-isFavourite(show.id);
+showFavourites();//refresh the favourites section
+isFavourite(show.id); //check if show is in favourites to update the hear icon
 document.getElementById("showInfo").style.display="block"
 console.log('User added:', data);
 console.log("YOu have clicked on "+show.name)
 }
 
-
+//function to remove favourites
 async function removefromFavourites(id){
      let res= await fetch(`http://localhost:3000/favourites/${id}`,{
           method: 'DELETE',
      });
      if (res.ok){
           alert("Removed from favourites!");
-          showFavourites();
-          isFavourite(id);
+          showFavourites();//refresh favourites
+          isFavourite(id);//update the heart icon
           document.getElementById("showInfo").style.display="block"
           console.log(`Show with ID ${id} was deleted.`);
         } else {
           console.error("Failed to delete favourite.");
         }
      }
-
+//function to search for show based on name
 async function searchMovie(moviename){
     const res= await fetch(`https://api.tvmaze.com/search/shows?q=${moviename}`)
     const data= await res.json()
     const movie= await data
     //showmovies(movie)
+
+    //store basic show information in seperate object (essentialData)
     let essentialData=movie.map((details)=>{
          return{
          id:details.show.id,
@@ -54,15 +56,16 @@ async function searchMovie(moviename){
          year:details.show.premiered?.split("-")[0] ?? "N/A"
          }
     })
-    document.getElementById("query-display").innerHTML="";
+    document.getElementById("query-display").innerHTML=""; //reset the query display section when a new search is entered
     essentialData.forEach((show)=>{
-         showmovies(show)
+         showmovies(show) //display the searched shows on the page
     })
      console.log(essentialData);
     }
     
     function showmovies(movie){
-    document.getElementById("showInfo").style.display="none"
+    document.getElementById("showInfo").style.display="none" //refresh the featured movie section.
+    //create card to diaply show
     queryDisplay=document.getElementById("query-display")
     moviecard=document.createElement("div")
     moviecard.className = "card movie-card";
@@ -75,19 +78,21 @@ async function searchMovie(moviename){
     <span class="card-type" id="card-type">${movie.year}</span>
     </div>
     `
+    //listen for click on each card from the search results
     moviecard.addEventListener("click",()=>{
     getFullInfo(movie.id);
     getcast(movie.id);
     })
-    
     queryDisplay.appendChild(moviecard)
     }
 
+    //function to fetch further info on tv show by id
     async function getFullInfo(id){
         console.log(`fetching for id:${id}`);
          const res=await fetch(`https://api.tvmaze.com/shows/${id}`)
          const show=await res.json()
    
+         //storing all the info in an object
          const fullInfo={
         id:String(show.id),
         name:show.name,
@@ -103,9 +108,11 @@ async function searchMovie(moviename){
         summary:show.summary,
          }
           // console.log(show);
-        displayShowInfo(fullInfo);
-         console.log(fullInfo);
+        displayShowInfo(fullInfo); //display show details on the showInfo section of the page
+     //     console.log(fullInfo);
 
+
+     //    Clone and replace heart to remove old click listeners
          const oldHeart = document.getElementById("heart");
          const newHeart = oldHeart.cloneNode(true);
          oldHeart.parentNode.replaceChild(newHeart, oldHeart);
@@ -115,8 +122,9 @@ async function searchMovie(moviename){
 
    }
 
+//function to display show deatils
    function displayShowInfo(show){
-   isFavourite(show.id);
+   isFavourite(show.id);//check if the show has already been added to favourites
    document.getElementById("genres").innerHTML=""
    document.getElementById("showInfo").style.display="block"
    document.getElementById("infoPoster").style.backgroundImage = `url(${show.image})`;
@@ -128,21 +136,20 @@ async function searchMovie(moviename){
         genreSpan=document.getElementById("genres")
         genreSpan.append(`  ${genre},`)
    }
-
-    
    }
 
+//function to fetch the show's cast based on the show id
 async function getcast(showId) {
     document.getElementById("showCast").innerHTML=""
     let res= await fetch(`https://api.tvmaze.com/shows/${showId}/cast`)
     let data=await res.json()
-    data.slice(0,10).map(actorData=>{
+    data.slice(0,10).map(actorData=>{// limit the result to only the first ten actors using slice
 
      let actor= actorData.person.name
      let character= actorData.character.name
      let image= actorData.person.image?.medium || 'default.png'
     
-    console.log(actor)
+//     console.log(actor)
     let showCast=document.getElementById("showCast")
     let actorCard=document.createElement("div")
     actorCard.className="actor-card card"
@@ -156,10 +163,9 @@ async function getcast(showId) {
 )
 }
 
-
-
-// })
    //searchMovie("silicon");
+
+//listener for form submition to initiate the search function.
    let searchform=document.querySelector("#searchform")
         searchform.addEventListener("submit",()=>{
         event.preventDefault();
@@ -167,7 +173,7 @@ async function getcast(showId) {
         searchMovie(searchInput);
    })
 
-
+//function to fetch and display the favourite shows from the local json server
    async function showFavourites() {
      try {
        const res = await fetch("http://localhost:3000/favourites");
@@ -192,8 +198,8 @@ async function getcast(showId) {
          `;
    
          moviecard.addEventListener("click", () => {
-           getFullInfo(show.id);
-           getcast(show.id);
+           getFullInfo(show.id);//show the clicked show on the featured section
+           getcast(show.id);//show the cast on the featured section
          });
    
          favouritesBody.appendChild(moviecard);
@@ -205,7 +211,7 @@ async function getcast(showId) {
    }
    
 
-
+//function to check if the show is already stored in the favourites
    async function isAlreadyFav(id,fullInfo) {
      try {
        const res = await fetch("http://localhost:3000/favourites");
@@ -214,17 +220,19 @@ async function getcast(showId) {
        const found = favourites.some(show => show.id == id);
    
        if (!found) {
-              addToFavourites(fullInfo)
+              addToFavourites(fullInfo) //show is added if not already there
          }
 
        else{
-          removefromFavourites(id);
+          removefromFavourites(id);//show is removed if already exists
        }
      } catch (error) {
        console.error("Error checking favourite:", error);
      }
    }
 
+
+// function to toggle the filled state of the heart icon
    async function isFavourite(id) {        
      try {
        const res = await fetch("http://localhost:3000/favourites");
@@ -237,7 +245,7 @@ async function getcast(showId) {
    
        if (found) {
          if (heartIcon) {
-           heartIcon.classList.add("bi-heart-fill");
+           heartIcon.classList.add("bi-heart-fill");//heart is filled when marked as favourite
            heartIcon.classList.remove("bi-heart");
            heartIcon.style.color = " rgb(238, 14, 208)";
          }
@@ -245,7 +253,7 @@ async function getcast(showId) {
        }else{
          if (heartIcon) {
            heartIcon.classList.add("bi-heart");
-           heartIcon.classList.remove("bi-heart-fill");
+           heartIcon.classList.remove("bi-heart-fill");//heart is not filled when not marked as favourite
            heartIcon.style.color = " rgb(238, 14, 208)";
          }
        }
@@ -256,7 +264,7 @@ async function getcast(showId) {
 
 
 
-showFavourites();
+showFavourites(); //show favourites imediately the page loads
 
 
 //    getcast(1719)   ;
